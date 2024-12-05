7d1e5fa60ccaa8fd21499f09d4898345cdb9de17
KJleN <yusupsh2006@gmail.com>
2024-12-05 09:54:20 +0300

    Update task1.c

--- 3824Б1ПР5/26 Шарапутдинов Юсуп Мухтарович/task1.c
+++ 3824Б1ПР5/26 Шарапутдинов Юсуп Мухтарович/task1.c
@@ -1,7 +1,23 @@
-// Задача 1. Найти сумму целых чисел a и b.
-// Если a + b >= INT_MAX, возвращать INT_MAX
-// Если a + b <= INT_MIN, возвращать INT_MIN
-int sum_of_two_numbers(int a, int b)
-{
-  return 0;
+#include <limits.h>
+/*#include<stdio,h>*/
+int sum_of_two_numbers(int a, int b) {
+  if (b > 0 && a > INT_MAX - b) {
+    return INT_MAX;
+  }
+  if (b < 0 && a < INT_MIN - b) {
+    return INT_MIN;
+  }
+  return a + b;
 }
+
+//Если нужно также выводить  сумму, добавляем: 
+
+/*int main() {
+    setlocale(LC_ALL, "Russian");
+    int a, b;
+    printf("Введите два целых числа: ");
+    scanf_s("%d %d", &a, &b);
+    int sum = sum_of_two_numbers(a, b);
+    printf("Сумма: %d\n", sum);
+    return 0;
+}*/
