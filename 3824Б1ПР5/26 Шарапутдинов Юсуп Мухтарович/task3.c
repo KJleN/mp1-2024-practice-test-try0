75579e95fb38ca9ab761b02a4963388d842c897d
KJleN <yusupsh2006@gmail.com>
2024-12-05 10:00:29 +0300

    Update task3.c

--- 3824Б1ПР5/26 Шарапутдинов Юсуп Мухтарович/task3.c
+++ 3824Б1ПР5/26 Шарапутдинов Юсуп Мухтарович/task3.c
@@ -1,5 +1,17 @@
-// Задача 3. Найти максимум из вешественных чисел a и b
-double max_of_two_numbers(double a, double b)
-{
-  return 0.0;
+/*#include <stdio.h>
+#include<locale.h>*/
+#include <math.h>
+
+double max_of_two_numbers(double a, double b) {
+    return fmax(a, b);
 }
+
+/*int main() {
+    setlocale(LC_ALL, "RUSSIAN");
+    double a, b;
+    printf("Введите два вещественных числа: ");
+    scanf_s("%lf %lf", &a, &b);
+    double max_num = max_of_two_numbers(a, b);
+    printf("Максимум: %lf\n", max_num);
+    return 0;
+}*/ 
